130742f57a4d29c74c270a2467264c7c374a8c6c
Jan Liam Arias <100700937+Grudge10@users.noreply.github.com>
2026-02-03 04:29:09 +0000

    removed old code

--- prelims/Week4/act2/Main.java
+++ prelims/Week4/act2/Main.java
@@ -3,8 +3,7 @@ package prelims.Week4.act2;
 public class Main {
     public static void main(String[] args) {
         while(true) {
-            System.out.println("back at main!");
-            SuperMain.start();
+            
         }
         
     }
