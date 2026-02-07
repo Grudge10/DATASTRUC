e362b512c4edbc9f64d53e8c9c74b9c6081ded18
Jan Liam Arias <100700937+Grudge10@users.noreply.github.com>
2026-02-07 06:37:23 +0000

    added necessary components for the activity

---
+++ prelims/Week4/activity7/Grocery.java
@@ -0,0 +1,106 @@
+package prelims.Week4.activity7;
+
+import java.util.*;
+
+public class Grocery {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String strProdName, strReceipt;
+        char cCustomer, cAnotherP;
+        double dQty, dBill, dPrice, dTotal, dPay, dChange;
+
+        do {
+            dBill = 0;
+            strReceipt = String.format("\n%-15s %-10s %-10s %-10s\n", "ITEM", "QTY", "PRICE", "TOTAL");
+            strReceipt += "------------------------------------------------------------\n";
+
+            System.out.println("\n****************************************");
+            System.out.println("         WELCOME TO THE STORE          ");
+            System.out.println("****************************************");
+
+            do {
+                System.out.println("\n--- Enter Item Details ---");
+                strProdName = getValidString(input, "Product Name: ");
+                dPrice = getValidDouble(input, "Price:        ", 0);
+                dQty = getValidDouble(input, "Quantity:     ", 1);
+
+                dTotal = dQty * dPrice;
+                dBill = dBill + dTotal;
+
+                strReceipt += String.format("%-15s %-10.2f %-10.2f %-10.2f\n", strProdName, dQty, dPrice, dTotal);
+
+                System.out.printf("Current Item Total: %.2f%n", dTotal);
+
+                cAnotherP = getValidYN(input, "Another product (Y/N)? ");
+            } while (cAnotherP == 'Y');
+
+            System.out.println("\n======================= FINAL RECEIPT ======================");
+            System.out.print(strReceipt);
+            System.out.println("------------------------------------------------------------");
+            System.out.printf("%-37s %.2f\n", "GRAND TOTAL:", dBill);
+            System.out.println("============================================================\n");
+
+            dPay = getValidDouble(input, "Payment Amount: ", dBill);
+
+            dChange = dPay - dBill;
+
+            System.out.println("\n-----------------------------------");
+            System.out.printf("CHANGE: %.2f%n", dChange);
+            System.out.println("Thank you for shopping!");
+            System.out.println("-----------------------------------");
+
+            cCustomer = getValidYN(input, "\nAnother customer (Y/N)? ");
+        } while (cCustomer == 'Y');
+
+        System.out.println("\nGrocery program terminating...");
+        input.close();
+    }
+
+    public static double getValidDouble(Scanner input, String prompt, double minValue) {
+        double value;
+        while (true) {
+            System.out.print(prompt);
+            if (input.hasNextDouble()) {
+                value = input.nextDouble();
+                input.nextLine();
+                if (value >= minValue) {
+                    return value;
+                } else {
+                    System.out.printf("Invalid! Must be at least %.2f: \n", minValue);
+                }
+            } else {
+                System.out.println("Invalid input! Please enter a number: ");
+                input.nextLine();
+            }
+        }
+    }
+
+    public static String getValidString(Scanner input, String prompt) {
+        String s;
+        while (true) {
+            System.out.print(prompt);
+            s = input.nextLine();
+            if (s.length() > 0) {
+                return s;
+            }
+            System.out.println("Input cannot be empty.");
+        }
+    }
+
+    public static char getValidYN(Scanner input, String prompt) {
+        String s;
+        char c;
+        while (true) {
+            System.out.print(prompt);
+            s = input.nextLine().toUpperCase();
+
+            if (s.length() > 0) {
+                c = s.charAt(0);
+                if (c == 'Y' || c == 'N') {
+                    return c;
+                }
+            }
+            System.out.println("Error! Invalid input. Please enter 'Y' for Yes or 'N' for No.");
+        }
+    }
+}
---
+++ prelims/Week4/activity7/Movie.java
@@ -0,0 +1,82 @@
+package prelims.Week4.activity7;
+
+import java.util.*;
+
+public class Movie {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int comedy = 0, horror = 0, scifi = 0, drama = 0, cartoon = 0, dvdTotal = 0, vcdTotal = 0, tapeTotal = 0,
+                rent = 0, sales = 0;
+        boolean choice = true;
+        while (choice) {
+            System.out.println("Registration");
+            System.out.println("1. DVD \n2. VCD \n3. Tape");
+            System.out.print("Choice: ");
+            int code = input.nextInt();
+            input.nextLine();
+
+            if (code == 1) {
+                System.out.println("Type: DVD");
+                dvdTotal += 1;
+            } else if (code == 2) {
+                System.out.println("Type: VCD");
+                vcdTotal += 1;
+            } else if (code == 3) {
+                System.out.println("Type: Tape");
+                tapeTotal += 1;
+            }
+            System.out.print("Title: ");
+            String title = input.nextLine();
+            System.out.println("1. Horror\n2. Scifi\n3. Drama\n4. Comedy \n5. Cartoons");
+            System.out.print("Category: ");
+            int category = input.nextInt();
+            input.nextLine();
+            if (category == 1) {
+                horror++;
+            } else if (category == 2) {
+                scifi++;
+            } else if (category == 3) {
+                drama++;
+            } else if (category == 4) {
+                comedy++;
+            } else if (category == 5) {
+                cartoon++;
+            }
+
+            System.out.print("Minutes: ");
+            int minutes = input.nextInt();
+            input.nextLine();
+
+            System.out.print("Setting: ");
+            String genre = input.nextLine();
+
+            System.out.println("1. Rental");
+            System.out.println("2. Sales");
+
+            System.out.print("Transaction: ");
+            int transactionType = input.nextInt();
+            input.nextLine();
+            if (transactionType == 1) {
+                rent++;
+            } else if (transactionType == 2) {
+                sales++;
+            }
+
+            System.out.print("Price: ");
+            double price = input.nextDouble();
+            input.nextLine();
+
+            System.out.print("Register another? ");
+            String userChoice = input.nextLine().toLowerCase();
+            char charUserChoice = userChoice.charAt(0);
+
+            choice = charUserChoice == 'n' ? false : true;
+        }
+
+        System.out.printf(
+                "Reports\nFor rent: %d\nFor sale: %d\nVCD Total: %d\nDVD Total: %d\nTape Total: %d\nHorror Movies: %d \nScifi Movies: %d\nDrama Movies: %d\nComedy Movies: %d\nCartoons: %d\n",
+                rent, sales, vcdTotal, dvdTotal, tapeTotal, horror, scifi, drama, comedy, cartoon);
+
+        input.close();
+    }
+}
