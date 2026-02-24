7ce63fa8be20f6b603e27d3abf4cddfaaca7594d
Jan Liam Arias <100700937+Grudge10@users.noreply.github.com>
2026-02-24 04:54:01 +0000

    starting the activity 2

--- midterms/Week1/GPURegister.java
+++ midterms/Week1/GPURegister.java
@@ -1,61 +1,30 @@
 package midterms.Week1;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class GPURegister {
+    public static String modelName, brand;
+    public static int vram, watts;
+    public static double price;
+
     public static void start(Scanner input) {
-        String modelName, brand;
-        int vram, watts, registeredGPU = 0;
-        double price;
+        List<GPURegister> gpu = new ArrayList<>();
 
         System.out.print("""
             
              ______________________________________
             |                                      |
             |   LIAM'S SUPERMALL GPU REGISTRATION  |
+            |           (Currently WIP!)           |
             |______________________________________|
             
             """);
-
-        do {
-            modelName = InputMethods.inputString("Model name: ", input, 28);
-            brand = InputMethods.inputString("Brand name: ", input, 28);
-            vram = InputMethods.inputInt("Amount of VRAM: ", input);
-            watts = InputMethods.inputInt("Watt consumption: ", input);
-            price = InputMethods.inputDouble("GPU price: ", input);
-
-            registeredGPU++;
-
-            printSummary(modelName, brand, vram, watts, price);
-
-            if (!InputMethods.yesOrNo("Would you like to register another GPU? (Y/N): ", input))
-                break;
-        } while (true);
-
-        System.out.printf("You have registered %d GPU%s!\n", registeredGPU, registeredGPU > 1 ? "'s" : "");
-
+            
         System.out.println("\nReturning to main menu...\n");
     }
 
-    public static void printSummary(String modelName, String brand, int vram, int watts, double price) {
-        System.out.printf("""
+    public static void loadList() {
 
-
-                You have registered a GPU!
-
-                 ___________________________________________
-                |                                           |
-                |                  Summary                  |
-                |___________________________________________|
-                |                                           |
-                |   Model name: %-28s|
-                |   Brand:      %-28s|
-                |   VRAM:       %-28d|
-                |   Watts:      %-28d|
-                |   Price:      %-28.2f|
-                |___________________________________________|
-
-
-                """, modelName, brand, vram, watts, price);
     }
 }
