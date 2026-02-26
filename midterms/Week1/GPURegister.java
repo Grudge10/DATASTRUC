06b77e9ba94ee62394b92e99f7318a8f582ac621
Jan Liam Arias <100700937+Grudge10@users.noreply.github.com>
2026-02-26 00:56:38 +0000

    made load method

--- midterms/Week1/GPURegister.java
+++ midterms/Week1/GPURegister.java
@@ -1,12 +1,15 @@
 package midterms.Week1;
 
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
 
 public class GPURegister {
-    public static String modelName, brand;
-    public static int vram, watts;
-    public static double price;
+    public String modelName, brand;
+    public int vram, watts;
+    public double price;
 
     public static void start(Scanner input) {
         List<GPURegister> gpu = new ArrayList<>();
@@ -24,7 +27,37 @@ public class GPURegister {
         System.out.println("\nReturning to main menu...\n");
     }
 
-    public static void loadList() {
+    public void loadList(List<GPURegister> gpu) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("midterms/Week1/GPU.txt"))) {
+            String line;
+            int counter = 1;
+            while ((line = reader.readLine()) != null) {
+                GPURegister loadGpu = new GPURegister();
 
+                switch (counter) {
+                    case 1:
+                        loadGpu.modelName = line;
+                        ++counter;
+                        break;
+                    case 2:
+                        loadGpu.
+                        ++counter;
+                        break;
+                    case 3:
+                        ++counter;
+                        break;
+                    case 4:
+                        ++counter;
+                        break;
+                    case 5:
+                        counter = 1;
+                        break;
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found. Starting from scratch!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
