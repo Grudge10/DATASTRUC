79d3c24289e225880116a0089cc12d59a928a2d8
Grudge10 <janliamarias2@gmail.com>
2026-04-21 23:44:03 +0800

    added in log in logic

--- finals/theact/Quizzer.java
+++ finals/theact/Quizzer.java
@@ -3,37 +3,101 @@ package finals.theact;
 import java.util.List;
 
 import finals.theact.models.Question;
+import finals.theact.models.User;
 import finals.theact.services.InputMethods;
 import finals.theact.services.QuizService;
+import finals.theact.services.UserService;
 
 public class Quizzer {
     public static void main(String[] args) {
+        User currentUser = null;
+        List<User> userList = UserService.loadUsers();
         List<Question> questionList = QuizService.loadQuestions();
 
-        boolean isRunning = true;
-        while (isRunning) {
-            System.out.println("""
-                     ____________________________________________________
-                    |                                                    |
-                    | [1] Player Registration                            |
-                    | [2] Play                                           |
-                    | [0] Exit                                           |
-                    |____________________________________________________|
-                    """);
-            int choice = InputMethods.inputInt("Choice: ", 0, 2);
+        boolean isRunning1 = true;
+        while (isRunning1) {
+            if (currentUser != null) {
+                System.out.println("""
+                         ____________________________________________________
+                        |                                                    |
+                        | [1] Logout                                         |
+                        | [2] Play                                           |
+                        | [0] Exit                                           |
+                        |____________________________________________________|
+                        """);
+            } else {
+                System.out.println("""
+                         ____________________________________________________
+                        |                                                    |
+                        | [1] Player Registration                            |
+                        | [2] Play                                           |
+                        | [0] Exit                                           |
+                        |____________________________________________________|
+                        """);
+            }
+            int choice1 = InputMethods.inputInt("Choice: ", 0, 2);
 
-            switch (choice) {
+            switch (choice1) {
                 case 1 -> {
-                    System.out.println("Player has registered!");
+                    if (currentUser != null) {
+                        currentUser = null;
+                        System.out.println("Successfully logged out!");
+                        break;
+                    }
+
+                    boolean isRunning2 = true;
+                    while (isRunning2) {
+                        System.out.println("""
+                                 ____________________________________________________
+                                |                                                    |
+                                | [1] Login                                          |
+                                | [2] Register                                       |
+                                | [0] Back                                           |
+                                |____________________________________________________|
+                                """);
+                        int choice2 = InputMethods.inputInt("Choice: ", 0, 2);
+
+                        switch (choice2) {
+                            case 1 -> {
+                                currentUser = UserService.login(userList);
+
+                                if (currentUser != null) {
+                                    System.out.println("You have successfully logged in!");
+                                    isRunning2 = false;
+                                }
+                            }
+                            case 2 -> {
+                                UserService.register(userList);
+                                System.out.println("Successfully Registered! Please Login.");
+                            }
+                            case 0 -> {
+                                isRunning2 = false;
+                            }
+                        }
+                    }
                 }
                 case 2 -> {
+                    if (currentUser == null) {
+                        System.out.println("ACCESS DENIED: You must Login or Register first");
+                        break;
+                    }
+
+                    System.out.println("Welcome " + currentUser.getUsername() + "!");
+
                     List<Question> selectedQuestions = QuizService.selectQuestions(questionList);
                     int score = QuizService.playGame(selectedQuestions);
+
+                    if (score > currentUser.getHighestScore()) {
+                        System.out.println("NEW HIGH SCORE!");
+                        currentUser.setHighestScore(score);
+                        UserService.saveUsers(userList); // Persist the change immediately!
+                    }
+
                     System.out.printf("your score is %d out of %d\n", score, selectedQuestions.size());
                 }
                 case 0 -> {
                     System.out.println("Exiting the program...");
-                    isRunning = false;
+                    isRunning1 = false;
                 }
             }
         }
---
+++ finals/theact/services/UserService.java
@@ -0,0 +1,112 @@
+package finals.theact.services;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import finals.theact.models.User;
+
+public class UserService {
+    private static final String USER_FILE_PATH = "finals/theact/data/Users.txt";
+
+    public static List<User> loadUsers() {
+        List<User> userList = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(USER_FILE_PATH))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] data = line.split(",");
+
+                String username = data[0];
+                String password = data[1];
+                int highestScore = 0;
+                try {
+                    highestScore = Integer.parseInt(data[2]);
+                } catch (NumberFormatException e) {
+                    e.printStackTrace();
+                }
+
+                User loadedUser = new User(username, password, highestScore);
+
+                userList.add(loadedUser);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found. Starting Fresh!");
+            return new ArrayList<>();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return userList;
+    }
+
+    public static void saveUsers(List<User> userList) {
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(USER_FILE_PATH))) {
+            StringBuilder s = new StringBuilder();
+
+            for (User user : userList) {
+                s.setLength(0);
+
+                s.append(user.getUsername())
+                        .append(",")
+                        .append(user.getPassword())
+                        .append(",")
+                        .append(user.getHighestScore());
+
+                writer.write(s.toString());
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static User login(List<User> userList) {
+        String username = InputMethods.inputString("username: ");
+        String password = InputMethods.inputString("password: ");
+
+        for (User user : userList) {
+            if (user.getPassword().equals(password) && user.getUsername().equals(username)) {
+                return user;
+            }
+        }
+
+        System.out.println("Invalid credentials!");
+        return null;
+    }
+
+    public static void register(List<User> userList) {
+        String username = "";
+        while (true) {
+            username = InputMethods.inputString("username: ");
+
+            boolean usernameExists = false;
+            for (User user : userList) {
+                if (username.equals(user.getUsername())) {
+                    usernameExists = true;
+                    break;
+                }
+            }
+
+            if (usernameExists) {
+                System.out.println("Username already exists!");
+            } else {
+                break;
+            }
+        }
+
+        String password = InputMethods.inputString("password: ");
+
+        User user = new User(username, password, 0);
+
+        userList.add(user);
+
+        saveUsers(userList);
+    }
+}
